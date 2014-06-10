060cbbf4795c3ef21056101cb8d77fe9f9cb3810
ehsun7b <ehsun7b@nl-04920.mimos.local>
2014-06-10 23:05:58 +0800

    init

---
+++ Server/ShaghayeghServer/src/main/java/anna/fsktm/shaghayeghserver/service/ContactService.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package anna.fsktm.shaghayeghserver.service;
+
+/**
+ *
+ * @author Ehsun Behravesh
+ */
+public class ContactService {
+    
+}
